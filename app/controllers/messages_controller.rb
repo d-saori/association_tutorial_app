5a298850080a9241edc3e44dff2ec4e14c8685d9
d-saori <60836449+d-saori@users.noreply.github.com>
2020-06-21 23:00:04 +0900

    fix: SQLパフォーマンス低下改善の為

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    # @messages = Message.all
+    # ↑の表記だとDB処理(view/messages/index.html.erb内messages.user.name)(message投稿時userテーブルから検索する動作が起こり処理負担が増えてしまう)
+    # SQLの結合(JOIN)という概念を利用
+    @messages = Message.includes(:user)
   end
 
   def new
