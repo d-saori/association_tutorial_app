a1ecd9b3acfd0c3558019ffcd668b3f9de6258b3
d-saori <60836449+d-saori@users.noreply.github.com>
2020-06-21 22:18:25 +0900

    fix: ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # TOPページ指定
+  root to: "messages/index"
+  # get 'messages/index'
+  # get 'messages/new'
+  # get 'messages/create'
+  resources :messages
   get 'users/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
